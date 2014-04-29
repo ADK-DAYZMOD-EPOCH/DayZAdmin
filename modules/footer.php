1cf9428d5f10ffefad2343298d1ad1fc31fff5df
raziel23x <raziel23x@gmail.com>
2014-04-29 17:08:50 -0400

    Updated Footer for ADK Servers

--- modules/footer.php
+++ modules/footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-	<?php echo $sitename ?> | <a href="https://github.com/R4Z0R49/DayZAdmin">DayZAdmin</a> Panel &copy; Copyright 2006-2013. Creator katzsmile. Redesigned by R4Z0R49/Wriley/Marcuz
+	<?php echo $sitename ?> | <a href="https://github.com/ADK-DAYZMOD-EPOCH/DayZAdmin">DayZAdmin</a> Panel &copy; Copyright 2006-2013. Creator katzsmile. Redesigned for DAYZ Epoch by &copy; Copyright 2014 ADK Gamers
 </div>
